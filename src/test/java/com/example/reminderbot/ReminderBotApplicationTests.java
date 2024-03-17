8cef5ac4b62b5fc1fedd925bc3cd85badb6c89a1
Andrey Nevedin <andrey.nevedin@raiffeisen.ru>
2024-03-17 18:42:42 +0300

    new functionality

--- src/test/java/com/example/reminderbot/ReminderBotApplicationTests.java
+++ src/test/java/com/example/reminderbot/ReminderBotApplicationTests.java
@@ -1,13 +1,10 @@
 package com.example.reminderbot;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+//@SpringBootTest
 class ReminderBotApplicationTests {
 
-	@Test
+/*	@Test
 	void contextLoads() {
-	}
+	}*/
 
 }
